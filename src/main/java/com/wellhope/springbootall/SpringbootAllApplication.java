27b4a7cdabbfe2f787b29431945270a7d2d2ec68
gaojie1209 <58129351+gaojie1209@users.noreply.github.com>
2021-02-28 02:07:37 +0800

    Update SpringbootAllApplication.java
    test

--- src/main/java/com/wellhope/springbootall/SpringbootAllApplication.java
+++ src/main/java/com/wellhope/springbootall/SpringbootAllApplication.java
@@ -3,7 +3,7 @@ package com.wellhope.springbootall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 @MapperScan("com.wellhope.springbootall.mapper")
 public class SpringbootAllApplication {
